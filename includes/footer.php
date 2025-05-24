793e3eb9ff3fb69ab336293ea2d12220efdb06f5
Sung Shin <jshin1223@gmail.com>
2025-05-24 21:31:09 +0900

    remove translate button in header.php

--- includes/footer.php
+++ includes/footer.php
@@ -20,11 +20,13 @@
     <!-- Google Translate integration script -->
     <script type="text/javascript">
         function googleTranslateElementInit() {
-            new google.translate.TranslateElement({
-                pageLanguage: 'en',
-                layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-                autoDisplay: false
-            }, 'google_translate_element');
+            if (!document.querySelector('#google_translate_element .goog-te-gadget')) {
+                new google.translate.TranslateElement({
+                    pageLanguage: 'en',
+                    layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+                    autoDisplay: false
+                }, 'google_translate_element');
+            }
         }
     </script>
     <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
--- includes/header.php
+++ includes/header.php
@@ -33,18 +33,6 @@ if (isset($_COOKIE['preferredFontSize'])) {
     <script src="/recipe-web-app/assets/js/scripts.js" defer></script>
 </head>
 
-<!-- Google Translate integration script -->
-<script type="text/javascript">
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement({
-        pageLanguage: 'en',
-        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-        autoDisplay: false
-    }, 'google_translate_element');
-}
-</script>
-<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-
 <!-- Apply font class to body from cookie -->
 <body class="<?php echo $fontClass; ?>">
 
